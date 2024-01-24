e1097f5caf562bcfdbfcf652ef8ac9772b4b1809
Siva Saravanan <sivasaravananrdm@gmail.com>
2024-01-24 23:19:36 +0530

    Updated red color

--- app/src/main/java/com/example/compose_auth/MainActivity.kt
+++ app/src/main/java/com/example/compose_auth/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
 fun MainApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        color = Color.Red
     ) {
         MainAppNavHost()
     }
